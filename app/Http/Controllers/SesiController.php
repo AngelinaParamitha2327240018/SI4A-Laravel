dcd07dda9224a038b69d243f9fb1a1a80bf60773
Angelina <angelinaparamitha385@gmail.com>
2025-06-01 16:32:53 +0700

    update sesi controller

--- app/Http/Controllers/SesiController.php
+++ app/Http/Controllers/SesiController.php
@@ -12,7 +12,8 @@ class SesiController extends Controller
      */
     public function index()
     {
-        //
+         $sesi = Sesi::all();
+        return view('sesi.index', compact('sesi'));
     }
 
     /**
@@ -20,7 +21,7 @@ class SesiController extends Controller
      */
     public function create()
     {
-        //
+        return view('sesi.create');
     }
 
     /**
@@ -28,15 +29,23 @@ class SesiController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $input = $request->validate([
+            'nama' => 'required'
+        ]);
+
+        Sesi::create($input);
+
+        return redirect()->route('sesi.index')
+            ->with('success', 'Sesi berhasil ditambahkan');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(sesi $sesi)
+    public function show($sesi)
     {
-        //
+        $sesi = Sesi::findOrFail($sesi);
+        return view('sesi.show', compact('sesi'));
     }
 
     /**
@@ -44,7 +53,7 @@ class SesiController extends Controller
      */
     public function edit(sesi $sesi)
     {
-        //
+        return view('sesi.edit', compact('sesi'));
     }
 
     /**
@@ -52,7 +61,11 @@ class SesiController extends Controller
      */
     public function update(Request $request, sesi $sesi)
     {
-        //
+        $input = $request->validate(['nama' => 'required']);
+        $sesi->update($input);
+
+        return redirect()->route('sesi.index')
+            ->with('success', 'Sesi berhasil diupdate');
     }
 
     /**
@@ -60,6 +73,12 @@ class SesiController extends Controller
      */
     public function destroy(sesi $sesi)
     {
-        //
+        $sesi = Sesi::findorfail($sesi);
+        //dd($sesi);
+
+        $sesi->delete();
+
+        return redirect()->route('sesi.index')
+            ->with('success', 'Sesi berhasil dihapus');
     }
 }
